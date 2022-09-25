da6b438b760c9474ddf98da4c038d3f23810726b
Shunta Ide <shuntaide16213@gmail.com>
2022-09-25 22:38:12 +0900

    Added POST method and comments

--- infrastructures/rounter.go
+++ infrastructures/rounter.go
@@ -13,12 +13,19 @@ var Router *gin.Engine
 func init() {
 	router := gin.Default()
 
+	/*
+		Users
+	*/
 	userController := controllers.NewUserController(*database.NewSqlHandler())
-
 	router.GET("/users/all", func(ctx *gin.Context) { userController.GetAllUser(context.TODO(), ctx) })
 	router.GET("/users/range", func(ctx *gin.Context) { userController.GetRangeUsers(context.TODO(), ctx) })
 	router.GET("/users", func(ctx *gin.Context) { userController.GetUsers(context.TODO(), ctx) })
 	router.GET("/user/:id", func(ctx *gin.Context) { userController.GetUser(context.TODO(), ctx) })
+	router.POST("/user", func(ctx *gin.Context) { userController.AddUser(context.TODO(), ctx) })
 
 	Router = router
+}
+
+type Input struct {
+	Name string `json:"name"`
 }
