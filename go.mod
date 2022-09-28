80f4f8f7176c0a795822babf07b16e65ea28293a
Shunta Ide <shuntaide16213@gmail.com>
2022-09-28 21:36:52 +0900

    Added: getUser test

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/shunta0213/go_test
 go 1.19
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.4.1
 	github.com/friendsofgo/errors v0.9.2
+	github.com/go-playground/assert/v2 v2.0.1
 	github.com/joho/godotenv v1.4.0
 	github.com/kat-co/vala v0.0.0-20170210184112-42e1d8b61f12
 	github.com/lib/pq v1.10.7
---
+++ test/user_gateway_test.go
@@ -0,0 +1,82 @@
+package test
+
+import (
+	"context"
+	"database/sql"
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-playground/assert/v2"
+	"github.com/shunta0213/go_test/adapters/gateways"
+	"github.com/shunta0213/go_test/entities"
+	"github.com/shunta0213/go_test/infrastructures/database"
+)
+
+func newDummySqlHandler(db *sql.DB) database.SqlHandler {
+	sqlHandler := new(database.SqlHandler)
+	sqlHandler.Conn = db
+	return *sqlHandler
+}
+
+func TestGetUser(t *testing.T) {
+
+	// Table for test
+
+	table := []struct {
+		testName string
+		id       int
+		user     entities.User
+		err      error
+	}{
+		{
+			"getUser",
+			20,
+			entities.User{
+				Id:        20,
+				Username:  "User",
+				Firstname: "first",
+				Lastname:  "last",
+				Email:     "email",
+				Contact:   "contact",
+			},
+			nil,
+		},
+	}
+
+	// db is for connecting mock db.
+	// mock is for setting mock.
+	db, mock, err := sqlmock.New()
+
+	if err != nil {
+		t.Error("SqlMock not Work")
+	}
+
+	defer db.Close()
+
+	repo := &gateways.UserGateway{
+		SqlHandler: newDummySqlHandler(db),
+	}
+
+	query := "SELECT * from users WHERE id=?"
+
+	for _, tt := range table {
+		t.Run(tt.testName, func(t *testing.T) {
+
+			b := tt.user
+
+			rows := sqlmock.NewRows([]string{
+				"id", "username", "firstname", "lastname", "email", "contact",
+			}).AddRow(b.Id, b.Username, b.Firstname, b.Lastname, b.Email, b.Contact)
+
+			// ExpectQuery mocks the combination of expected SELECT statement and retrieved result.
+			mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(tt.id).WillReturnRows(rows)
+
+			got, err := repo.GetUser(context.TODO(), b.Id)
+
+			assert.Equal(t, tt.err, err)
+			assert.Equal(t, b, got)
+		})
+	}
+
+}
